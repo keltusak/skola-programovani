9f14254dc0cdb49f9427aa081f2627c360976975
keltusak <135325525+keltusak@users.noreply.github.com>
2024-10-04 13:40:37 +0200

    přidání funkce času

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 import tkinter as tk
 from tkinter import HORIZONTAL, TOP, LEFT
+from os.path import exists
+from datetime import datetime
+
 
 # from tkinter import ttk
 
@@ -31,10 +34,10 @@ class Application(tk.Tk):
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
-        self.bind("<Escape>", self.quit)
+        self.bind("<Escape>", self.destroy)
         self.lblMain = tk.Label(self, text="Color Mishmash")
-        self.btnQuit = tk.Button(self, text="Save & Quit", command=self.quit)
-        self.btnClear = tk.Button(self, text="Clear colors", command=self.clearColors)
+        self.lblMain.bind("<Button-1>", self.timestringHandler)
+        self.btnQuit = tk.Button(self, text="Quit", command=self.quit)
         self.frameR = tk.Frame(master=self)
         self.frameG = tk.Frame(self)
         self.frameB = tk.Frame(self)
@@ -44,7 +47,7 @@ class Application(tk.Tk):
         self.lblG = tk.Label(self.frameG, text="G", width=2)
         self.lblB = tk.Label(self.frameB, text="B", width=2)
 
-        self.varR = tk.IntVar(value=255)
+        self.varR = tk.IntVar()
         self.varR.trace_add("write", self.updateColor)
         self.scaleR = tk.Scale(
             self.frameR,
@@ -56,7 +59,7 @@ class Application(tk.Tk):
         )
         self.entryR = tk.Entry(self.frameR, textvariable=self.varR, width=3)
 
-        self.varG = tk.IntVar(value=255)
+        self.varG = tk.IntVar()
         self.varG.trace_add("write", self.updateColor)
         self.scaleG = tk.Scale(
             self.frameG,
@@ -68,7 +71,7 @@ class Application(tk.Tk):
         )
         self.entryG = tk.Entry(self.frameG, textvariable=self.varG, width=3)
 
-        self.varB = tk.IntVar(value=255)
+        self.varB = tk.IntVar()
         self.varB.trace_add("write", self.updateColor)
         self.scaleB = tk.Scale(
             self.frameB,
@@ -80,7 +83,7 @@ class Application(tk.Tk):
         )
         self.entryB = tk.Entry(self.frameB, textvariable=self.varB, width=3)
 
-        self.canvas = tk.Canvas(self, background="#ffffff", width=30)
+        self.canvas = tk.Canvas(self, background="#FF5588", width=30)
         self.canvas.bind("<Button-1>", self.clickHandler)
 
         self.lblMain.pack(side=TOP)
@@ -103,17 +106,18 @@ class Application(tk.Tk):
         self.canvas.pack(side=TOP, fill="both")
         self.frameMem.pack(side=TOP, fill="both")
 
-        self.canvasList = []
+        self.canvaslist = []
         for row in range(3):
             for col in range(7):
-                canvas = tk.Canvas(self.frameMem, width=50, height=50, bg="#ffffff")
+                canvas = tk.Canvas(self.frameMem, width=50, height=50, bg="#12abc3")
                 canvas.bind("<Button-1>", self.clickHandler)
                 canvas.grid(row=row, column=col)
-                self.canvasList.append(canvas)
+                self.canvaslist.append(canvas)
 
         self.btnQuit.pack()
-        self.btnClear.pack()
-        self.load()
+        self.colorLoad()
+        # self.protocol("WM_DELETE_WINDOW", self.colorSave)
+        self.timestring()
 
     def clickHandler(self, event):
         if self.cget("cursor") != "pencil":
@@ -137,28 +141,47 @@ class Application(tk.Tk):
         b = self.scaleB.get()
         self.canvas.config(background=f"#{r:02X}{g:02X}{b:02X}")
 
-    def quit(self, event=None):
-        self.save()
-        super().quit()
+    def colorSave(self):
+        with open("colors.txt", "w") as f:
+            f.write(self.canvas.cget("background") + "\n")
+            for c in self.canvaslist:
+                f.write(c.cget("background") + "\n")
 
-    def save(self):
-        colors = []
-        for canvas in self.canvasList:
-            colors.append(canvas.cget("background"))
+    def colorLoad(self):
+        if not exists("colors.txt"):
+            return
+        with open("colors.txt", "r") as f:
+            try:
+                color = f.readline().strip()
+                r = int(color[1:3], 16)
+                g = int(color[3:5], 16)
+                b = int(color[5:], 16)
+                self.varR.set(r)
+                self.varG.set(g)
+                self.varB.set(b)
+                for canvas in self.canvaslist:
+                    canvas.config(background=f.readline().strip())
+            except (ValueError, tk.TclError):
+                pass
 
-        file = open('latestColors.txt', 'w')
-        file.write(str.join("\n", colors))
+    def timestring(self):
+        current_time = datetime.now()
+        string = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        self.lblMain.configure(text=string)
+        self.timestring_id = self.after(1000, self.timestring)
 
-    def load(self):
-        file = open('latestColors.txt', 'r')
-        for i, color in enumerate(file.readlines()):
-            if self.canvasList[i]:
-                self.canvasList[i].config(background=str.replace(color, "\n", ""))
+    def timestringHandler(self, event):
+        if self.timestring_id:
+            self.after_cancel(self.timestring_id)
+            self.timestring_id=None
+        else:
+            self.timestring()
 
-    def clearColors(self):
-        for canvas in self.canvasList:
-            canvas.config(background="#FFFFFF")
+    def destroy(self, event=None):
+        self.colorSave()
+        super().destroy()
 
 
 app = Application()
 app.mainloop()
+
